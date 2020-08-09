755b24e8c1502d4de97de0b6cf2dd54074f3fdad
Mike Calleja <hello@mikecalleja.com>
2020-08-09 17:35:52 +0000

    V1 of star table

--- models/stackoverflow/main_q_sme.sql
+++ models/stackoverflow/main_q_sme.sql
@@ -8,7 +8,6 @@ with unanswered_query_type as (
 
     select DISTINCT * from {{ ref('index_query') }}
     WHERE community_owned_date IS NULL
-    AND answer_count = 0
     AND accepted_answer_id IS NULL
     AND creation_date > '2020-05-01T00:00:00+00:00'
 
@@ -21,12 +20,19 @@ subject_matter_experts as(
 joined as (
     SELECT DISTINCT
     u.title,
-    s.tags,
+    u.creation_date,
+    u.last_activity_date,
+    u.answer_count,
+    u.tags,
+    u.view_count,
+    u.accepted_answer_id,
     s.sme_user_id
     from subject_matter_experts s
     INNER JOIN unanswered_query_type u
     ON u.tags = s.tags
+    
 )
 
 select * from joined
+order by 6 DESC
 LIMIT 100
